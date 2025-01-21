8e589ecd27f07e2bef14944ea6cad901805f1fe1
Merlijn Van Waeyenberghe <20674257-merlinfromBE@users.noreply.replit.com>
2025-01-21 13:14:25 +0000

    Assistant checkpoint: Add banner image to header
    Assistant generated file changes:
    - templates/index.html: Add banner image and improve header styling
    - static/app.js: Update header to include banner image
    
    ---
    
    User prompt:
    
    In stead of the title, use this image as a banner
    https://private-user-images.githubusercontent.com/43906965/383288071-c036ff17-ecad-4615-a7b5-6ffbd3d4ebf1.png

--- static/app.js
+++ static/app.js
@@ -13,7 +13,9 @@ function App() {
         <div className="min-h-screen bg-gray-100">
             <header className="bg-white shadow">
                 <div className="max-w-7xl mx-auto py-6 px-4">
-                    <h1 className="text-3xl font-bold text-gray-900">IntuneBrew</h1>
+                    <img src="https://private-user-images.githubusercontent.com/43906965/383288071-c036ff17-ecad-4615-a7b5-6ffbd3d4ebf1.png" 
+                         alt="IntuneBrew Banner" 
+                         className="banner" />
                 </div>
             </header>
             <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
